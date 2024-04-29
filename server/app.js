7bb06cf2ef6b1af7df09ca82feaea46d0546def5
Milad Naeini <milad88884@gmail.com>
2024-04-29 18:11:01 +0200

    fix: route

--- server/app.js
+++ server/app.js
@@ -4,20 +4,24 @@ const cors = require("cors");
 const app = express();
 
 //Import Routes
-const getRoutes = require("./routes/gets");
-const postRoutes = require("./routes/posts");
-const putRoutes = require("./routes/puts");
-const deleteRoutes = require("./routes/deletes");
+// const getRoutes = require("./routes/gets");
+// const postRoutes = require("./routes/posts");
+// const putRoutes = require("./routes/puts");
+// const deleteRoutes = require("./routes/deletes");
 
 //Middlewares
-app.use(cors());
+// app.use(cors());
 //CORS allows you to load resources from different origins
 
 //ROUTES
-app.use("/api/words", getRoutes);
-app.use("/add", postRoutes);
-app.use("/add", putRoutes);
-app.use("/words", deleteRoutes);
+// app.use("/api/words", getRoutes);
+// app.use("/add", postRoutes);
+// app.use("/add", putRoutes);
+// app.use("/words", deleteRoutes);
+const router = express.Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
 
 //Start listening to the server
 module.exports.handler = serverless(app);
