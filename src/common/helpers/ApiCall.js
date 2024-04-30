8d0e8e370923dbbe8fb814f8b134c0578f72cfb5
Milad Naeini <milad88884@gmail.com>
2024-04-30 09:28:22 +0200

    chages api to ngrok

--- src/common/helpers/ApiCall.js
+++ src/common/helpers/ApiCall.js
@@ -1,17 +1,19 @@
 import axios from "axios";
+const api = axios.create();
+api.defaults.headers["ngrok-skip-browser-warning"] = 1234;
 
 export const postApiCall = async (url, body) => {
-  return await axios.post(url, body);
+  return await api.post(url, body);
 };
 
 export const putApiCall = async (url, body) => {
-  return await axios.put(url, body);
+  return await api.put(url, body);
 };
 
 export const deleteApiCall = async (url) => {
-  return await axios.delete(url);
+  return await api.delete(url);
 };
 
 export const getApiCall = async (url) => {
-  return await axios.get(url);
+  return await api.get(url);
 };
