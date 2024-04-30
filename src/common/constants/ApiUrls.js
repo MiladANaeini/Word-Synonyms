db96bf0ec49178951247c84f7f26e013ada0314b
Milad Naeini <milad88884@gmail.com>
2024-04-30 10:33:10 +0200

    api

--- src/common/constants/ApiUrls.js
+++ src/common/constants/ApiUrls.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "https://words-synonym-server.vercel.app";
-export const BASE_URL = "https://elegant-killdeer-absolutely.ngrok-free.app"; //base URL for local
+export const BASE_URL = "http://142.93.139.88:5000"; //base URL for local
 export const SEARCH_WORD_URL = BASE_URL + "/words";
 export const ADD_NEW_WORD_OR_SYNONYM_URL = BASE_URL + "/add";
