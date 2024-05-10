285b18c1a6ac79c9efe7f6b6a3ae82752f58908d
Milad Naeini <milad88884@gmail.com>
2024-05-10 14:27:11 +0200

    file clear

--- src/common/hooks/useFetchData.js
+++ src/common/hooks/useFetchData.js
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
 import { getApiCall } from "../../common/helpers/ApiCall";
 
-const useFetchData = ({
-  url = "",
-  callBack = () => {},
-  enable = true,
-  //no query params to avoid complexity given that we dont have complex filters
-}) => {
+const useFetchData = ({ url = "", callBack = () => {}, enable = true }) => {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
